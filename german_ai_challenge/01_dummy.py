016545bdd98e7b518a4a835fa4792d71d25e6e81
zkf85 <zkf1985@gmail.com>
2018-12-07 16:48:11 +0800

    a

--- german_ai_challenge/01_dummy.py
+++ german_ai_challenge/01_dummy.py
@@ -1,52 +1,50 @@
-############################################################
+#################################################################
 # Alibaba Cloud German AI Challenge 2018
 # KF 2018/12/06
 #    2018/12/07
-############################################################
-
-############################################################
-# I. Imports and Paths
-############################################################
+#################################################################
 import os
 import numpy as np
 import h5py
 import tensorflow as tf
 import csv
 
+# Print title with double lines with text aligned to center
+def print_title(title):
+    print('')
+    print('=' * 65)
+    print(' ' * ((65 - len(title))//2 - 1), title)
+    print('=' * 65)
 
+#################################################################
+# I. Paths
+#################################################################
 # Set paths
 base_dir = os.path.expanduser('/home/kefeng/German_AI_Challenge/dataset')
-
 path_training = os.path.join(base_dir, 'training.h5')
 path_validation = os.path.join(base_dir, 'validation.h5')
 path_round1_test = os.path.join(base_dir,'round1_test_a_20181109.h5')
 
 # Validate the paths
-print('')
-print('='*60)
-print(' '*20, 'Paths')
-print('='*60)
+print_title('Paths')
 print("Validate data in the base directory : ")
 print(os.listdir(base_dir))
 print('')
 
-############################################################
+#################################################################
 # II. Overview of the Data
-############################################################
+#################################################################
 fid_training = h5py.File(path_training, 'r')
 fid_validation = h5py.File(path_validation, 'r')
 fid_test1 = h5py.File(path_round1_test, 'r')
 
 # Have a look at keys stored in the h5 files
-print('')
-print('='*60)
-print(' '*20, "Overview of Data:")
-print('='*60)
+print_title("Overview of Data:")
 print('Training data keys   :', list(fid_training.keys()))
 print('Validation data keys :', list(fid_validation.keys()))
 print('Round1 Test data keys :', list(fid_test1.keys()))
 
-print('-'*60)
+print('-'*65)
 print("Training data shapes:")
 s1_training = fid_training['sen1']
 s2_training = fid_training['sen2']
@@ -54,7 +52,7 @@ label_training = fid_training['label']
 print('  Sentinel 1 data shape :', s1_training.shape)
 print('  Sentinel 2 data shape :', s2_training.shape)
 print('  Label data shape      :', label_training.shape)
-print('-'*60)
+print('-'*65)
 print("Validation data shapes:")
 s1_validation = fid_validation['sen1']
 s2_validation = fid_validation['sen2']
@@ -68,9 +66,17 @@ s2_test1 = fid_test1['sen2']
 print('  Sentinel 1 data shape :', s1_test1.shape)
 print('  Sentinel 2 data shape :', s2_test1.shape)
 
+# Save input dimension parameters
+input_width = s1_training.shape[1]
+input_height = s1_training.shape[2]
+s1_channel = s1_training.shape[3]
+s2_channel = s2_training.shape[3]
+
+#################################################################
 # III. Build Model
+#################################################################
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(),
+    tf.keras.layers.Flatten(input_shape=((input_width, input_height, s1_channel + s2_channel))),
     tf.keras.layers.Dense(256, activation=tf.nn.relu),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(17, activation=tf.nn.softmax)
@@ -80,12 +86,12 @@ model.compile(optimizer='adam',
                 loss='binary_crossentropy',
                 metrics=['accuracy'])
 
+print_title("Model Summary")
 model.summary()
 
-############################################################
+#################################################################
 # IV. Train Data Flow
-############################################################
-
+#################################################################
 # Set flag for test mode or not
 #is_test = True
 is_test = False
@@ -94,43 +100,43 @@ is_test = False
 epochs = 3
 
 # Set data flow block size 
-block_size = pow(2, 13)   
+block_size = pow(2, 14)   
 
 # Set batch_size 
-batch_size = 64
+batch_size = 128
 
 if is_test:
     train_size = 30000
     val_size = 10000
+    epochs = 3    
+    block_size = 4096
+    batch_size = 32
 
 else:
     train_size = len(label_training)
     val_size = len(label_validation)
 
 print('')
-print('')
-print('='*60)
-print(' '*20, "Start Training")
-print('='*60)
+print_title("Start Training")
 
 # Training loop
 for e in range(epochs):
     print("")
-    print("-"*60)
+    print("-"*65)
     print("[KF INFO] EPOCH :", e + 1)
-    print("-"*60)
+    print("-"*65)
 
     for i in range(0, train_size, block_size):
-        if i % 10 == 0:
+        if i % (5*block_size) == 0:
             print("[KF INFO] EPOCH %d >> data trained : %d/%d" % (e + 1, i, train_size))
         start_pos = i
         end_pos = min(i + block_size, train_size)
         train_s1_x_block = np.asarray(s1_training[start_pos:end_pos])
         train_s2_x_block = np.asarray(s2_training[start_pos:end_pos])
         train_y_block = np.asarray(label_training[start_pos:end_pos])
+        # concatenate s1 and s2 data along the last axis
         train_concat_x_block = np.concatenate([train_s1_x_block, train_s2_x_block], axis=-1) 
-
-        #print(train_concat_x_block.shape, train_y_block.shape)
+        # training process
         model.fit(train_concat_x_block, train_y_block, epochs=1, batch_size=batch_size)
 
     # Validate for each epoch
@@ -145,12 +151,11 @@ for e in range(epochs):
         val_s1_x_block = np.asarray(s1_validation[start_pos:end_pos])
         val_s2_x_block = np.asarray(s2_validation[start_pos:end_pos])
         val_y_block = np.asarray(label_validation[start_pos:end_pos])
-        
+        # concatenate s1 and s2 data along the last axis
         val_concat_x_block = np.concatenate([val_s1_x_block, val_s2_x_block], axis=-1) 
-        #print(val_concat_x_block.shape, train_y_block.shape)
-
+        # validation process
         val_res = model.evaluate(val_concat_x_block, val_y_block, batch_size=batch_size)
-        # Cumulate the loss and accuracy
+        # cumulate the loss and accuracy
         loss += val_res[0]*(end_pos - i)
         acc_num += val_res[1]*(end_pos - i)
 
@@ -160,13 +165,16 @@ print('')
 print("[KF INFO] Training Completed!")
 print('')
 
+#################################################################
 # V. Predict
-print('')
-print('='*60)
-print(' '*10, "Predicting with round 1 test data")
-print('='*60)
+#################################################################
+print_title("Predicting with round 1 test data")
+
+# concatenate s1 and s2 data along the last axis
 test_concat = np.concatenate([s1_test1, s2_test1], axis=-1)
+# predicting process
 res = model.predict(test_concat)
+# find the largest confident's index
 res = np.argmax(res, axis=1)
 
 # Convert categorical result to one-hot encoded matrix
@@ -179,11 +187,8 @@ print("[KF INFO] Prediction shape:", final_res.shape)
 from datetime import datetime
 
 cur_date = datetime.now()
-csv_name = 'prediction-%d%d%d-epochs-%d-trainsize-%d' % (cur_date.year, cur_date.month, cur_date.day, epochs, train_size)
+csv_name = 'prediction-%d%d%d-epochs-%d-trainsize-%d.csv' % (cur_date.year, cur_date.month, cur_date.day, epochs, train_size)
 pred_dir = 'predictions'
 
-with open(os.path.join(pred_dir, csv_name), 'w') as f:
-    writer = csv.writer(f)
-    for line in final_res:
-        writer.writerow(line)
-    print('[KF INFO] Prediction csv saved!')
+np.savetxt(os.path.join(pred_dir, csv_name), final_res, fmt='%d', delimiter=',')
+print('[KF INFO] Prediction csv saved!')
